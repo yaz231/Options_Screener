3103c495a87327751bce947e525e935c6daafa82
Yazan Alatrach <yazanalatrach@Yazans-MacBook-Pro.local>
2024-01-06 03:14:05 -0600

    Updated Options Builder

--- app/main.py
+++ app/main.py
@@ -254,7 +254,7 @@ def get_start_date():
 async def get_strike_prices(symbol: str, request: Request, db: Session = Depends(get_db)):
     session_id = request.state.session_id
     strike_prices = db.query(Option.strike).filter(Option.session_id == session_id, Option.symbol == symbol).distinct().all()
-    return {"strikePrices": strike_prices}
+    return {"strikePrices": [price[0] for price in strike_prices]}
 
 def create_stock_chart(data, symbol, timeframe):
     fig = go.Figure(data=[go.Candlestick(x=data.index,
